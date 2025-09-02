4591a8fbcc335a10651880f6617fc0f234244872
enriquevillon25 <enriquevillon2597@gmail.com>
2025-09-02 01:18:16 -0500

    feat

--- next.config.ts
+++ next.config.ts
@@ -1,21 +1,8 @@
 import type { NextConfig } from "next";
 import path from "node:path";
 
-const nextConfig: NextConfig = {
-  output: "export",
-  trailingSlash: true,
-  images: { unoptimized: true },
-
-  // Para GH Pages (Project Pages):
-  // Tu sitio vive en /portfolio, así que:
-  basePath: "/portfolio",
-
-  // Silencia el warning del workspace root
-  outputFileTracingRoot: path.resolve(process.cwd()),
-
-  // Solo para desplegar rápido; luego puedes quitarlos
-  eslint: { ignoreDuringBuilds: true },
-  typescript: { ignoreBuildErrors: true },
+const nextConfig = {
+  // para SSR en Vercel NO pongas output: "export"
+  // y puedes usar <Image> con optimización normal
 };
-
 export default nextConfig;
